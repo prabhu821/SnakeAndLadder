d6538093fbf72364a3be3feef8b2c82399479482
rishiraj007 <rishiraj@mounttalent.com>
2022-12-04 17:04:57 +0530

    [Prabhu]: Snake and Ladder

---
+++ Snake-Ladder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Snake_Ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
