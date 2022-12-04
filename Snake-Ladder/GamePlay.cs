8a716375044517425337559e8244e9b38282e1f0
rishiraj007 <rishiraj@mounttalent.com>
2022-12-04 17:40:52 +0530

    [Prabhu]: Player Position Zero Restart

--- Snake-Ladder/GamePlay.cs
+++ Snake-Ladder/GamePlay.cs
@@ -53,6 +53,41 @@ namespace Snake_Ladder
         }
 
 
+        public void PlayerPositionZeroRestart()
+        {
+            //local variable
+            int rollDice, playerPosition = 0;
+
+            //To generate random number
+            Random random = new Random();
+            //List to check options
+            var list = new List<string> { "No Play", "Ladder", "Snake" };
+
+            //Conditions for user options
+            rollDice = random.Next(1, 6);
+            Console.WriteLine("Dice number is : " + rollDice);
+
+            //Print user options
+            Console.WriteLine("Player checks options 1.No Play  2.Ladder   3.Snake");
+
+            //Use random to check user option
+            int index = random.Next(list.Count);
+            Console.WriteLine("Player option is : " + list[index]);
+            if (playerPosition + rollDice < 100)
+            {
+                if (list[index] == "Ladder") playerPosition += rollDice;
+                if (list[index] == "Snake") playerPosition -= rollDice;
+            }
+
+            if (playerPosition < 0)
+            {
+                playerPosition = 0;
+            }
+            //Print to Console
+            Console.WriteLine("Player current position : " + playerPosition);
+            Console.WriteLine("Final position is :" + playerPosition);
+        }
+
 
     }
 }
