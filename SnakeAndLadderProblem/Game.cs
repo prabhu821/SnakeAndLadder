ff0fc20cbac1c49822bdf8e86a6ae3858274adbe
rishiraj007 <rishiraj@mounttalent.com>
2023-06-09 19:00:19 +0530

    [Prabhu]Add: player1 roll dice

--- SnakeAndLadderProblem/Game.cs
+++ SnakeAndLadderProblem/Game.cs
@@ -10,10 +10,19 @@ namespace SnakeAndLadderProblem
     {
         public int position = 0;
         public int player1;
+        static Random random = new Random();
+        int diceValue = random.Next(1, 7);
+
         //UC1- Game played with single player at start position 0
         public void GameStart()
         {
             Console.WriteLine("Single player at start position is : " + position);
+        }
+
+        //UC2- Player rolls the dice to get a number between 1 to 6.
+        public void PlayerRollsDice()
+        {
+            Console.WriteLine("Player 1 roll dice then get number: " + diceValue);
         }
     }
 }
--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -8,13 +8,16 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nSelect option \n1.GameStart \n2.Exit");
+                Console.WriteLine("\nSelect option \n1.Start Game \n2.Player1 roll dice \n3.Exit");
                 int options = Convert.ToInt32(Console.ReadLine());
                 Game game = new Game();
                 switch (options)
                 {
                     case 1:
                         game.GameStart();
+                        break;
+                    case 2:
+                        game.PlayerRollsDice();
                         break;
                     default:
                         flag = false;
