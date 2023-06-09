4ad3f9b8ce76989cf70930666bc455d2042f3952
rishiraj007 <rishiraj@mounttalent.com>
2023-06-09 18:50:57 +0530

    [Prabhu]Add: Single player start at position zero

---
+++ SnakeAndLadderProblem/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem
+{
+    public class Game
+    {
+        public int position = 0;
+        public int player1;
+        //UC1- Game played with single player at start position 0
+        public void GameStart()
+        {
+            Console.WriteLine("Single player at start position is : " + position);
+        }
+    }
+}
--- SnakeAndLadderProblem/Program.cs
+++ SnakeAndLadderProblem/Program.cs
@@ -5,6 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Problem !");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nSelect option \n1.GameStart \n2.Exit");
+                int options = Convert.ToInt32(Console.ReadLine());
+                Game game = new Game();
+                switch (options)
+                {
+                    case 1:
+                        game.GameStart();
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
